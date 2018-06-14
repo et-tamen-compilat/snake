b28ecaf0cd715d2d0721611f49eea8e16833fa7e
Hashan Punchihewa <hsp17@ic.ac.uk>
2018-06-14 09:24:11 +0000

    Reorganised code

--- snake.c
+++ snake.c
@@ -18,8 +18,11 @@ bool intersects(snake_t s, point_t p);
 bool point_equal(point_t p1, point_t p2);
 point_t direct_point(point_t p, direction d);
 snake_t *create_snake();
+wall_t *create_wall();
+colour_t multicolour(int pos);
+colour_t get_default(int pos);
+point_t get_food(snake_t *snake, wall_t* wall_arr);
 
-#define NUM_WALLS 20
 volatile sig_atomic_t done = 0;
 volatile struct RGBLedMatrix *matrix;
 
@@ -30,138 +33,6 @@ void term(int signum) {
 }
 
 int TIME_AFTER_DEATH = -1;
-//Checks to see if two points are equal
-// Returns true if will be out of bounds
-bool illegal_direction(direction curr_d, direction new_d) {
-  return (
-      (curr_d == RIGHT && (new_d == LEFT || new_d == RIGHT)) ||
-      (curr_d == LEFT && (new_d == RIGHT || new_d == LEFT)) ||
-      (curr_d == UP && (new_d == DOWN || new_d == UP)) ||
-      (curr_d == DOWN && (new_d == UP || new_d == DOWN))
-      );
-}
-
-//Checks to see if two nodes are equal
-bool node_equal(node_t n1, node_t n2){
-  return (point_equal(n1.point, n2.point) && n1.next == n2.next);
-}
-
-
-bool food_wall(point_t point, int **collision_map){
-  return (collision_map[point.x][point.y]);
-}
-void add_wall_to_map(int **map, wall_t *wall) {
-  printf("x val: %i, y val: %i\n", wall->start.x, wall->start.y);
-  for (int i = 0; i < wall->length; i++) {
-  printf("x val: %i, y val: %i\n", wall->start.x, wall->start.y);
-    switch (wall->direction) {
-      case UP: 
-        map[wall->start.x][wall->start.y - i] = 1;
-        break;
-      case DOWN:
-        map[wall->start.x][wall->start.y + i] = 1;
-        break;
-      case LEFT:
-        map[wall->start.x - i][wall->start.y] = 1;
-        break;
-      case RIGHT:
-        map[wall->start.x + i][wall->start.y] = 1;
-        break;
-    }
-  }
-}
-
-
-int **create_collision_map(wall_t *wall_arr) {
-  int **map = malloc(sizeof(int *) * MAX_WIDTH);
-  for (int i = 0; i < MAX_WIDTH; i++) {
-    map[i] = calloc(MAX_HEIGHT, sizeof(int));
-  }
-//  memset(map, 0, sizeof(int) * MAX_WIDTH * MAX_HEIGHT);
-  for (int i = 0; i < NUM_WALLS; i++) {
-    add_wall_to_map(map, &(wall_arr[i]));
-  }
-  return map; 
-}
-
-
-point_t get_food(snake_t *snake, wall_t* wall_arr) {
-  point_t point;
-  do {
-    point.x = rand() % MAX_WIDTH;
-    point.y = rand() % MAX_HEIGHT;
-  } while(intersects(*snake, point) || food_wall(point, create_collision_map(wall_arr)));
-  return point;
-}
-
-colour_t get_default(int pos) {
-  colour_t b = {0, 255, 0};
-  return b;
-}
-
-colour_t colours[] = {
-  { 255, 0, 0 },
-  { 0, 0, 255 },
-  { 175, 65, 244 },
-  { 65, 244, 238 },
-  { 232, 126, 99 },
-  { 249, 154, 222 },
-  { 192, 249, 154 }
-};
-
-colour_t multicolour(int pos) {
-  return colours[pos % 7];
-}
-
-//returns random int in range
-int get_rand_int(int min, int max) {
-  return rand() % (max + 1 - min) + min;
-}
-
-direction get_rand_dir() {
-  int num = get_rand_int(0, 1);
-  switch (num) {
-    case 0: return DOWN; break;
-    case 1: return RIGHT; break;
-    default: break;
-  }
-}
-
-// Makes sure the wall doesn't go off of the map
-int calc_length(point_t start, direction d) {
-  switch (d) {
-    case UP: 
-      return start.y > WALL_MAX_LEN ? WALL_MAX_LEN : start.y;
-      break;
-    case DOWN:
-      return MAX_HEIGHT - start.y > WALL_MAX_LEN ? WALL_MAX_LEN :
-        MAX_HEIGHT - start.y;
-      break;
-    case LEFT:
-      return start.x > WALL_MAX_LEN ? WALL_MAX_LEN : start.x;
-      break;
-    case RIGHT:
-      return MAX_WIDTH - start.x > WALL_MAX_LEN ? WALL_MAX_LEN :
-        MAX_WIDTH - start.x;
-      break;
-  }
-}
-
-wall_t *create_wall() {
-  wall_t *wall = malloc(sizeof(wall_t));
-  wall->length = get_rand_int(WALL_MIN_LEN, WALL_MAX_LEN);
-  wall->direction = get_rand_dir();
-
-  int x = MAX_WIDTH - 1, y = MAX_HEIGHT - 1;
-  switch (wall->direction) {
-    case RIGHT: x -= wall->length;
-    case DOWN: y -= wall->length;
-  }
-
-  wall->start = (point_t) {get_rand_int(0, x), get_rand_int(0, y)};
-  wall->colour = WALL_COLOUR;
-  return wall;
-}
 
 void draw_wall(struct LedCanvas *canvas, wall_t *wall) {
   int x0 = wall->start.x;
@@ -237,43 +108,6 @@ void draw_snake(struct LedCanvas *canvas, snake_t *s, colour_function_t *c, poin
   if (k % 10 == 0 && TIME_AFTER_DEATH == -1) {
     led_canvas_set_pixel(canvas, food.x, food.y, 255, 0, 0);
   }
-}
-
-bool perform_move(snake_t *snake, direction d, point_t* food, wall_t *walls) {
-  if (out_bounds(snake->tail->point, d)) {
-    return false; 
-  }
-  //  printf("%p\n", snake->head);
-  //  snake->head = snake->head->next;
-  point_t p = direct_point(snake->tail->point, d);
-  for (int i = 0; i < NUM_WALLS; i++) {
-    point_t point = walls[i].start;
-    for (int j = 0; j < walls[i].length; j++) {
-      if (point_equal(point, p)) {
-        return false;
-      }
-      switch (walls[i].direction) {
-        case DOWN: point.y++; break;
-        case RIGHT: point.x++; break;
-      }
-    }
-  }
-  bool eq = point_equal(*food, p);
-  if (!eq) {
-    snake->head = snake->head->next;
-    if (intersects(*snake, p)) {
-      return false;
-    } 
-  } 
-  node_t *nova = malloc(sizeof(node_t));
-  *nova = (node_t) { p, NULL };
-  snake->tail->next = nova;
-  snake->tail = nova;
-  if (eq) {
-    *food = get_food(snake, walls);
-    snake->length++;
-  }
-  return true;
 }
 
 input input_init(struct js_event e) {
