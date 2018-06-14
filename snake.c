863a3bdc09baf7d824ba07f79d6fc5f53393cd5d
jackmorrison <jsm1317@ic.ac.uk>
2018-06-14 15:31:29 +0100

    Created method to play sounds

--- snake.c
+++ snake.c
@@ -203,6 +203,42 @@ static long get_milliseconds() {
   return (t.tv_sec * 1000) + (t.tv_usec / 1000);
 }
 
+//Takes in a number corresponding to the sound to play
+void play_sound(int i){
+  int pid;
+  pid=fork();
+  if(pid==0)
+  {
+    printf("I am the child\n");
+    switch(i){
+      case 1:
+        execpl("/usr/bin/omxplayer", " ", "arm11_24/snake/sounds/classic.m4a", NULL);
+        break;
+      case 2:
+        execpl("/usr/bin/omxplayer", " ", "arm11_24/snake/sounds/crazy.m4a", NULL);
+        break;
+      case 3:
+        execpl("/usr/bin/omxplayer", " ", "arm11_24/snake/sounds/menu.m4a", NULL);
+        break;
+      case 4:
+        execpl("/usr/bin/omxplayer", " ", "arm11_24/snake/sounds/eat.wav", NULL);
+        break;
+      case 5:
+        execpl("/usr/bin/omxplayer", " ", "arm11_24/snake/sounds/die.wav", NULL);
+        break;
+      case 6:
+        execpl("/usr/bin/omxplayer", " ", "arm11_24/snake/sounds/pause.wav", NULL);
+        break;
+    }
+    _exit(0);
+  }
+  else
+  {
+    printf("I am the parent\n");
+    wait();
+  } 
+}
+
 #define MAX(a, b) (a < b ? b : a)
 
 int main(int argc, char **argv) {
